ef7159e325dcb559cde9d0af1a8f031077e967f2
interntuts <117482814+interntuts@users.noreply.github.com>
2023-03-18 09:34:58 +0530

    Update views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,11 +3,6 @@ from .models import Employees
 from django.contrib import messages
 # Create your views here.
 
-#fetch employee all data
-def grid(request):
-    employees = Employees.objects.all()
-    return render(request, 'grid.html',{"employees":employees})
-
 # add employee data
 def add(request):
     if request.method == "POST":
@@ -25,29 +20,3 @@ def add(request):
             messages.error(request, e)
 
     return render(request, 'add.html')
-
-def edit(request,id):
-    if request.method == "POST":
-       employee = Employees.objects.get(pk=id)
-       employee.name = request.POST["name"]
-       employee.designation = request.POST["designation"]
-       employee.age = request.POST["age"]
-       employee.gender = request.POST["gender"]
-       employee.save()
-       messages.success(request, "employee updated successfully")
-       return redirect('grid')
-    else:
-        employee = Employees.objects.get(pk=id)
-        gender = ["male","female"]
-        return render(request, "edit.html",{"employee":employee,"gender":gender})
-
-# delete employee
-def delete(request,id):
-    employee = Employees.objects.get(pk=id)
-    if employee:
-        delete = employee.delete()
-        messages.success(request, "Employee deleted successfully")
-        return redirect('grid')
-    
-    messages.error(request, "Unable to delete")
-    return redirect('grid')
