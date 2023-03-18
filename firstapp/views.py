96bb2f59cb717c3820f9f4c521b57e3543425f83
interntuts <117482814+interntuts@users.noreply.github.com>
2023-03-18 09:38:10 +0530

    Update views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,20 +3,18 @@ from .models import Employees
 from django.contrib import messages
 # Create your views here.
 
-# add employee data
-def add(request):
+# edit employee data
+def edit(request,id):
     if request.method == "POST":
-        try:
-            form_data = {
-            "name": request.POST["name"], 
-            "designation": request.POST["designation"], 
-            "age": request.POST["age"], 
-            "gender": request.POST["gender"], 
-            }
-            employee = Employees(**form_data)
-            employee.save()
-            messages.success(request, "Employee added successfully")
-        except Exception as e:
-            messages.error(request, e)
-
-    return render(request, 'add.html')
+       employee = Employees.objects.get(pk=id)
+       employee.name = request.POST["name"]
+       employee.designation = request.POST["designation"]
+       employee.age = request.POST["age"]
+       employee.gender = request.POST["gender"]
+       employee.save()
+       messages.success(request, "employee updated successfully")
+       return redirect('grid')
+    else:
+        employee = Employees.objects.get(pk=id)
+        gender = ["male","female"]
+        return render(request, "edit.html",{"employee":employee,"gender":gender})
